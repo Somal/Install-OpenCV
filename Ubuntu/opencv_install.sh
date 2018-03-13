d397713c47a5679f783afdd09147fcfc71f03cdc
Maxim Sokolov <somal1996@rambler.ru>
2018-03-13 20:53:44 +0300

    Update opencv_install.sh

--- Ubuntu/opencv_install.sh
+++ Ubuntu/opencv_install.sh
@@ -43,28 +43,21 @@ cd opencv-$version
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D BUILD_DOCS=OFF \
-      -D BUILD_TESTS=OFF \
-      -D BUILD_EXAMPLES=OFF \
-      -D BUILD_opencv_python2=OFF \
-      -D BUILD_opencv_python3=ON \
-      -D WITH_1394=OFF \
-	  -D WITH_MATLAB=OFF \
-	  -D WITH_OPENCL=OFF \
-	  -D WITH_OPENCLAMDBLAS=OFF \
-	  -D WITH_OPENCLAMDFFT=OFF \
-	  -D CMAKE_CXX_FLAGS="-O3 -funsafe-math-optimizations" \
-	  -D CMAKE_C_FLAGS="-O3 -funsafe-math-optimizations" \
-      -D WITH_TBB=ON \
-      -D WITH_NVCUVID=OFF \
-      -D WITH_CUDA=OFF \
-      -D BUILD_NEW_PYTHON_SUPPORT=ON \
-      -D WITH_V4L=OFF \
-      -D INSTALL_C_EXAMPLES=OFF \
-      -D INSTALL_PYTHON_EXAMPLES=OFF \
-      -D WITH_QT=OFF \ 
-      -D WITH_OPENGL=ON ..
+		-D CMAKE_INSTALL_PREFIX=/usr/local \
+		-D INSTALL_PYTHON_EXAMPLES=OFF \
+		-D OPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib-3.2.0/modules \
+		-D BUILD_DOCS=OFF \
+		-D BUILD_TESTS=OFF \
+		-D BUILD_EXAMPLES=OFF \
+		-D BUILD_opencv_python2=OFF \
+		-D BUILD_opencv_python3=ON \
+		-D WITH_1394=OFF \
+		-D WITH_MATLAB=OFF \
+		-D WITH_OPENCL=OFF \
+		-D WITH_OPENCLAMDBLAS=OFF \
+		-D WITH_OPENCLAMDFFT=OFF \
+		-D CMAKE_CXX_FLAGS="-O3 -funsafe-math-optimizations" \
+		-D CMAKE_C_FLAGS="-O3 -funsafe-math-optimizations" ..
 make -j 4
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
